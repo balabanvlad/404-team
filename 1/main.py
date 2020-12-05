6505875b93071a5248d526930345eab107918210
vladislav <balaban-2000@list.ru>
2020-12-05 19:28:05 +0200

    small bug fix

--- 1/main.py
+++ 1/main.py
@@ -3,11 +3,11 @@ from PIL import Image
 img = Image.open('best.bmp')
 
 SizeX, SizeY = img.size
-
 suma = 0
-
-for i in range(1, SizeX):
-    for j in range(1, SizeY):
+k = 0
+for i in range(0, SizeX):
+    for j in range(0, SizeY):
+        k+=1
         pixVal = img.getpixel((i, j))
         suma += pixVal[0]
 
